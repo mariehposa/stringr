0ea8f51fe98e02657184b57507a49cbdda0914e2
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-20 16:21:08 +0100

    add texts for header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from './components/navigation/naviagtion';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
       <Navigation />
+      <Header />
     </div>
   );
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
+import '../../App.css';
+import downArrow from '../img/down-arrow.svg';
 
 export default function Header() {
     return (
         <div>
-
+            <div>
+                <p>Video of anything. anywhere.</p>
+                <p> String has a network of over 125,000 videographers to get you the footage you want, when you need it.</p>
+                <button>Learn more</button>
+                <a href="/what_is_stringr">
+                    <img src={downArrow} alt="what is stringr" />
+                </a>
+            </div>
+            <ReactPlayer
+                className='react-player'
+                url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+            />
+           
         </div>
     )
 }
