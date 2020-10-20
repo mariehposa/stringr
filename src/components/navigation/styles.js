180bf29843fd7a8657d63902839f92aeeabda639
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-20 12:30:19 +0100

    add styles for nav

---
+++ src/components/navigation/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const StyledNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: #1b242f;
+  border-bottom: 3px solid #04c2c9;
+  padding: 0.5rem;
+  padding-left: 4rem;
+`;
+
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+  margin: auto 0;
+  margin-left: 1.6rem;
+  color: white;
+
+  &:hover {
+    color: #e31b6d;
+    transition: 0.3s ease-out;
+  }
+`;
+
