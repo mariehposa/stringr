892fda728bf097fc0bfd7e9a4e2b4a0744aef812
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-20 19:35:02 +0100

    add styles for header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,23 +2,25 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import '../../App.css';
 import downArrow from '../img/down-arrow.svg';
+import {StyledDiv, StyledNav, StyledH, StyledP, StyledButton, StyledImg} from './styles';
 
 export default function Header() {
     return (
         <div>
-            <div>
-                <p>Video of anything. anywhere.</p>
-                <p> String has a network of over 125,000 videographers to get you the footage you want, when you need it.</p>
-                <button>Learn more</button>
-                <a href="/what_is_stringr">
-                    <img src={downArrow} alt="what is stringr" />
-                </a>
-            </div>
+            <StyledDiv>
+                <StyledH>Video of anything. anywhere.</StyledH>
+                <StyledP> Stringr has a network of over 125,000 videographers to get you the footage you want, when you need it.</StyledP>
+                <StyledButton>Learn more</StyledButton>
+                <StyledNav to="/what_is_stringr">
+                    <StyledImg src={downArrow} alt="what is stringr" />
+                </StyledNav>
+            </StyledDiv>
             <ReactPlayer
                 className='react-player'
                 url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                width="100%"
+                height="90vh"
             />
-           
         </div>
     )
 }
---
+++ src/components/header/styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const StyledDiv = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    color: wheat;
+    top: 40%;
+    bottom: 1%;
+`;
+
+export const StyledH = styled.h1`
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: 800;
+    text-align: center;
+    font-size: 3.4rem;
+`;
+
+export const StyledP = styled.p`
+    text-align: center;
+    color: #fff;
+    /* text-shadow: 0px 1px 2px rgba(0,0,0,.6); */
+    font-weight: 300;
+    /* padding: 20px 0; */
+    line-height: 1.4;
+    font-size: 1.8rem;
+    width: 55%;
+    margin-top: -10px;
+    line-height: 50px;
+`;
+
+export const StyledNav = styled(NavLink)`
+    
+`;
+
+export const StyledButton = styled.button`
+    background: #cd0c0c;
+    color: #fff;
+    border: 0;
+    padding: 10px 50px;
+    font-size: 15px;
+    text-transform: uppercase;
+    font-weight: 600;
+    text-shadow: none;
+    border-radius: 3px;
+
+    &:hover {
+        color: #ef4a34;
+        transition: color .3s ease;
+    }
+`;
+
+export const StyledImg = styled.img`
+    width: 20px;
+    margin-top: 20px;
+`;
