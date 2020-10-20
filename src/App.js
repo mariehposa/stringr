50c72aa1813b11a45aee821039e52789f72ce02a
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-20 12:11:25 +0100

    import nav into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from './components/navigation/naviagtion';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navigation />
     </div>
   );
 }
