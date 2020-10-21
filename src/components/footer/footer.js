2fada02168a5440eb64d6742abb3225cd6965931
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-21 21:32:02 +0100

    add styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyledAll, StyledDiv, StyledDiv1, StyledDiv2, StyledNav, StyledA } from './styles';
+import { StyledAll, StyledDiv, StyledNav, StyledA, StyledDiv1 } from './styles';
 import facebook from '../img/facebook.png';
 import twitter from '../img/twitter.png';
 
-export default function Faq() {
+export default function Footer() {
     return (
         <StyledAll>
             <StyledDiv>
@@ -11,20 +11,20 @@ export default function Faq() {
             </StyledDiv>
 
             <StyledDiv1>
+                <StyledNav to="/">Terms of service</StyledNav>
+                <StyledNav to="/">Privacy policy</StyledNav>
+                <StyledNav to="/">cookie preferences</StyledNav>
+                <StyledNav to="/">contact us</StyledNav>
+            </StyledDiv1>
+
+            <div>
                 <StyledA  title="facebook" href="https://www.facebook.com/contactstringr" target="_blank" rel="noopener noreferrer">
                     <img src={facebook} alt="stringr facebook" />
                 </StyledA>
                 <StyledA  title="gmail profile" href="https://twitter.com/ContactStringr" target="_blank" rel="noopener noreferrer">
                     <img src={twitter} alt="stringr twitter" />
                 </StyledA>
-            </StyledDiv1>
-
-            <StyledDiv2>
-                <StyledNav>Terms of service</StyledNav>
-                <StyledNav>Privacy policy</StyledNav>
-                <StyledNav>cookie preferences</StyledNav>
-                <StyledNav>contact us</StyledNav>
-            </StyledDiv2>
+            </div>
         </StyledAll>
     )
 }
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -2,25 +2,37 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledAll = styled.div`
-
+    text-align: center;
+background-color: #050505;
+padding: 100px 0;
 `
 
 export const StyledDiv = styled.div`
-
-`
-
-export const StyledDiv1 = styled.div`
-
-`
-
-export const StyledDiv2 = styled.div`
-
+    border-right: 1px solid #777;
+background: #fff;
+font-size: 18px;
+width: 380px;
+color: #777;
+text-transform: uppercase;
+margin: auto;
 `
 
 export const StyledNav = styled(NavLink)`
+    color: #777;
+font-size: .8rem;
+font-weight: 400;
+text-transform: uppercase;
+padding: 0;
+margin: 5px;
+padding-right: 10px;
+border-right: 1px solid #777;
+text-decoration: none;
+`
 
+export const StyledDiv1 = styled.div`
+    margin: 20px 0;
 `
 
 export const StyledA = styled.a`
-
+    margin: 10px 5px;
 `
