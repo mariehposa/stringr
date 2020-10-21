c004eea82a68f2999812c52b518cc74a238facd7
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-21 19:20:15 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import WhatIsStringr from './components/what_is_stringr/what_is_stringr';
 import HowItWorks from './components/how_it_works/how_it_works';
 import AboutUs from './components/about_us/about_us';
 import Faq from './components/faq/faq';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <HowItWorks />
       <AboutUs />
       <Faq />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const StyledAll = styled.div`
+
+`
+
+export const StyledDiv = styled.div`
+
+`
+
+export const StyledDiv1 = styled.div`
+
+`
+
+export const StyledDiv2 = styled.div`
+
+`
+
+export const StyledNav = styled(NavLink)`
+
+`
+
+export const StyledA = styled.a`
+
+`
