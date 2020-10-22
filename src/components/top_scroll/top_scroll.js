6afa8b82b52ca127829b6800956071172a97e19b
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-22 14:18:50 +0100

    wip

---
+++ src/components/top_scroll/top_scroll.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+export default class ScrollToTop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      is_visible: false,
+    };
+  }
+
+  componentDidMount() {
+    var scrollComponent = this;
+    document.addEventListener("scroll", function (e) {
+      scrollComponent.toggleVisibility();
+    });
+  }
+
+  toggleVisibility() {
+    if (window.pageYOffset > 300) {
+      this.setState({
+        is_visible: true,
+      });
+    } else {
+      this.setState({
+        is_visible: false,
+      });
+    }
+  }
+
+  scrollToTop() {}
+
+  render() {
+    const { is_visible } = this.state;
+    return (
+      <div className="scroll-to-top">
+        {is_visible && (
+          <div onClick={() => this.scrollToTop()}>
+              <p>ScrollUp</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
